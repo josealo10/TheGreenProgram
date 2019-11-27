7995fca2dc68324026adaca5d39e531e70b12061
Jose Alonso Alfaro Perez <josealonso.alfahotmail.com@Joses-Mac.local>
2019-11-27 01:17:27 -0600

    Initial Commit

---
+++ TheGreenProgramTests/TheGreenProgramTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheGreenProgramTests.swift
+//  TheGreenProgramTests
+//
+//  Created by Jose Alonso Alfaro Perez on 11/27/19.
+//  Copyright © 2019 Jose Alonso Alfaro Perez. All rights reserved.
+//
+
+import XCTest
+@testable import TheGreenProgram
+
+class TheGreenProgramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
