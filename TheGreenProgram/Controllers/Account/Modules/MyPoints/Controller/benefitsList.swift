b087ae5166e2f0f0039a334de8a2844c76d8413e
Alonso <josealonso.alfa@gmail.com>
2020-06-02 11:00:07 -0600

    IOS

--- TheGreenProgram/Controllers/Account/Modules/MyPoints/Controller/benefitsList.swift
+++ TheGreenProgram/Controllers/Account/Modules/MyPoints/Controller/benefitsList.swift
@@ -10,21 +10,61 @@ import UIKit
 
 class benefitsList: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var levelImage: UIImageView!
+    var level:LoyaltyLevel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.separatorColor = .white
+        levelLabel.text = level?.name
+        levelImage.backgroundColor = UIColor().colorFromHex(level?.color ?? "#0000")
+        tabBarController?.tabBar.isHidden = true
+        configureNavigationBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureNavigationBar(){
+        navigationItem.largeTitleDisplayMode = .never
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.primaryTextColor]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.primaryTextColor]
+        navBarAppearance.backgroundColor = .colorPrimary
+        navigationItem.standardAppearance = navBarAppearance
+        navigationItem.scrollEdgeAppearance = navBarAppearance
+        navigationItem.title = ""
     }
-    */
+}
 
+//MARK:- Extension
+extension benefitsList:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return level?.benefits?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let benefit = UILabel()
+        benefit.text = "• " + (level?.benefits?[indexPath.row])!
+        benefit.translatesAutoresizingMaskIntoConstraints = false
+        
+        cell.addSubview(benefit)
+        benefit.leftAnchor.constraint(equalTo: cell.leftAnchor, constant: 20).isActive = true
+        benefit.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    
 }
--- TheGreenProgram/Utils/Alerts.swift
+++ TheGreenProgram/Utils/Alerts.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import UIKit
+
+struct Alert {
+    
+    static func showBasicAlert(on vc: UIViewController, with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.async { vc.present(alert, animated: true) }
+    }
+    
+    static func showDecitionAlert (on vc: UIViewController, with title: String, message: String, acceptHandler: @escaping () -> Void, cancelHandler: @escaping () -> Void){
+    
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.destructive, handler: {
+            (action) in alert.dismiss(animated: true, completion: nil)
+            cancelHandler()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertAction.Style.default, handler: {
+            (action) in alert.dismiss(animated: true, completion: nil)
+            acceptHandler()
+        }))
+        
+        
+        
+        DispatchQueue.main.async { vc.present(alert, animated: true) }
+    }
+    
+    static func showDecitionAlert (on vc: UIViewController, with title: String, message: String, acceptTitle:String, acceptHandler: @escaping () -> Void, cancelTitle:String, cancelHandler: @escaping () -> Void){
+    
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: acceptTitle, style: UIAlertAction.Style.default, handler: {
+            (action) in alert.dismiss(animated: true, completion: nil)
+            acceptHandler()
+        }))
+        
+        alert.addAction(UIAlertAction(title: cancelTitle, style: UIAlertAction.Style.destructive, handler: {
+            (action) in alert.dismiss(animated: true, completion: nil)
+            cancelHandler()
+        }))
+        
+        DispatchQueue.main.async { vc.present(alert, animated: true) }
+    }
+    
+}
